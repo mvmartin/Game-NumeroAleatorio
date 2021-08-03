f1a9757e60a4f4aec582de3d60da3a331d652d1b
Marcos Martins <mvmartiins@hotmail.com>
2021-08-03 16:52:41 -0300

    Add files via upload

---
+++ Game.js
@@ -0,0 +1,23 @@
+<script>
+
+function pularLinha() {
+        document.write("<br>");
+    }
+
+    function mostra(frase){
+        document.write(frase);
+        pularLinha();
+    }
+
+    var numeroPensado = Math.round(Math.random() * 10)
+    var chute = parseInt(prompt("Digite seu chute!"));
+
+    if(chute == numeroPensado){
+        mostra("Você acertou Miseraví!")
+    }   else{
+        mostra("Você Errou, o número pensado foi " + numeroPensado)
+    }
+
+
+
+</script>
